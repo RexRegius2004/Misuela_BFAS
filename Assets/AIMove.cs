f591bb537909237c47bc58df829f7e13a3daaeaf
RexRegius2004 <134357506+RexRegius2004@users.noreply.github.com>
2024-07-08 10:54:42 +0800

    BFAS

---
+++ Assets/AIMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour
+{
+    List<Tile> selectableTiles = new();
+    GameObject[] tiles;
+    public int move = 5;
+    public float jumpHeight = 2;
+    public float moveSpeed;
+    Tile currentTile;
+
+    Vector3 velocity = new();
+    Vector3 heading = new();
+
+    float halfHeight = 0;
+    protected void Init()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        halfHeight = GetComponent<Collider>().bounds.extents.y;
+    } 
+
+    public void GetCurrentTile()
+    {
+        currentTile = GetTargetTile(gameObject);
+        currentTile.current = true;
+
+    }
+    
+    public Tile GetTargetTile(GameObject target)
+    {
+
+    }
+}
